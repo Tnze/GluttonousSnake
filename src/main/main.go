ca6bf287ef8de4dff0eabf734746e8c459ee2115
GeniusJunDao <cjd001113@outlook.com>
2018-02-11 16:12:31 +0800

    使游戏的刷新不再依赖帧数

--- src/main/main.go
+++ src/main/main.go
@@ -10,18 +10,17 @@ import (
 func main() {
 	snake := gs.NewSnake()
 	direction := 0
-	t := 0
+	t := time.Now()
 	draw.OpenWindow(func() *gs.Snake {
-		if t > 10 {
+		if time.Now().Sub(t)/time.Millisecond > 250 {
 			score, isEnd := snake.Step(direction)
 			fmt.Printf("\rscore:	%d", score)
-			t = 0
 			if isEnd {
 				snake = gs.NewSnake()
 				direction = 0
 			}
+			t = time.Now()
 		}
-		t++
 		time.Sleep(20 * time.Millisecond)
 		return snake
 	}, func(dir int) {
